43ab3054fa9ec9e6f9daa92a4432cf6498c98a8b
Abdelrahman Nasr <abdelrhmannasr275@gmail.com>
2025-07-05 23:20:44 +0300

    Add project files.

---
+++ OOP Assignment01/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace OOP_Assignment01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Days of the week are:");
+
+            foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+           
+             {
+               Console.WriteLine(day);
+             }
+            
+
+        }
+    }
+}
